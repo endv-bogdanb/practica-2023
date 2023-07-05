078d5f865fbf9669a893a5aa41498c4a593f0e6e
Vlad Andreica <Vandreica@192-168-0-200.rdsnet.ro>
2023-07-05 15:19:12 +0300

    Add Update functionality to orders

--- src/components/createEvent.js
+++ src/components/createEvent.js
@@ -1,4 +1,4 @@
-import { addPurchase, kebabCase } from '../utils';
+import { addPurchase, kebabCase } from "../utils";
 
 /**
  *
@@ -12,21 +12,21 @@ export const createEvent = ({
   name,
   ticketCategories,
 }) => {
-  const eventDiv = document.createElement('div');
+  const eventDiv = document.createElement("div");
 
   const title = kebabCase(name);
 
   eventDiv.classList.add(
-    'event',
-    'bg-white',
-    'rounded',
-    'shadow-md',
-    'p-4',
-    'flex',
-    'flex-col',
-    'm-6',
-    'mt-8',
-    'width-400'
+    "event",
+    "bg-white",
+    "rounded",
+    "shadow-md",
+    "p-4",
+    "flex",
+    "flex-col",
+    "m-6",
+    "mt-8",
+    "width-400"
   );
 
   eventDiv.innerHTML = `
@@ -35,8 +35,8 @@ export const createEvent = ({
     <p class="description text-gray-700">${description}</p>
   `;
 
-  const actions = document.createElement('div');
-  actions.classList.add('actions', 'flex', 'items-center', 'mt-4');
+  const actions = document.createElement("div");
+  actions.classList.add("actions", "flex", "items-center", "mt-4");
 
   const categoriesOptions = ticketCategories.map(
     (ticketCategory) =>
@@ -46,38 +46,38 @@ export const createEvent = ({
   actions.innerHTML = `
     <h2 class="text-lg font-bold mb-2">Choose Ticket Type:</h2>
     <select id="ticketType" name="ticketType" class="select ${title}-ticket-type border border-gray-300 rounded py-2 px-3 bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-      ${categoriesOptions.join('\n')}
+      ${categoriesOptions.join("\n")}
     </select>
   `;
 
-  const quantity = document.createElement('div');
-  quantity.classList.add('quantity', 'flex', 'items-center', 'ml-4');
+  const quantity = document.createElement("div");
+  quantity.classList.add("quantity", "flex", "items-center", "ml-4");
 
-  const input = document.createElement('input');
+  const input = document.createElement("input");
   input.classList.add(
-    'input',
-    'w-16',
-    'text-center',
-    'border',
-    'border-gray-300',
-    'rounded',
-    'py-2',
-    'px-3',
-    'text-gray-700',
-    'focus:outline-none',
-    'focus:shadow-outline'
+    "input",
+    "w-16",
+    "text-center",
+    "border",
+    "border-gray-300",
+    "rounded",
+    "py-2",
+    "px-3",
+    "text-gray-700",
+    "focus:outline-none",
+    "focus:shadow-outline"
   );
-  input.type = 'number';
-  input.min = '0';
-  input.value = '0';
+  input.type = "number";
+  input.min = "0";
+  input.value = "0";
 
-  input.addEventListener('blur', () => {
+  input.addEventListener("blur", () => {
     if (!input.value) {
       input.value = 0;
     }
   });
 
-  input.addEventListener('input', () => {
+  input.addEventListener("input", () => {
     if (parseInt(input.value)) {
       addToCart.disabled = false;
     } else {
@@ -87,28 +87,28 @@ export const createEvent = ({
 
   quantity.appendChild(input);
 
-  const quantityActions = document.createElement('div');
+  const quantityActions = document.createElement("div");
   quantityActions.classList.add(
-    'quantity-actions',
-    'flex',
-    'space-x-2',
-    'ml-6'
+    "quantity-actions",
+    "flex",
+    "space-x-2",
+    "ml-6"
   );
 
-  const increase = document.createElement('button');
+  const increase = document.createElement("button");
   increase.classList.add(
-    'increase',
-    'px-3',
-    'py-1',
-    'rounded',
-    'add-btn',
-    'text-white',
-    'hover:bg-green-300',
-    'focus:outline-none',
-    'focus:shadow-outline'
+    "increase",
+    "px-3",
+    "py-1",
+    "rounded",
+    "add-btn",
+    "text-white",
+    "hover:bg-green-300",
+    "focus:outline-none",
+    "focus:shadow-outline"
   );
-  increase.innerText = '+';
-  increase.addEventListener('click', () => {
+  increase.innerText = "+";
+  increase.addEventListener("click", () => {
     input.value = parseInt(input.value) + 1;
     if (parseInt(input.value)) {
       addToCart.disabled = false;
@@ -117,20 +117,20 @@ export const createEvent = ({
     }
   });
 
-  const decrease = document.createElement('button');
+  const decrease = document.createElement("button");
   decrease.classList.add(
-    'decrease',
-    'px-3',
-    'py-1',
-    'rounded',
-    'bg-red-700',
-    'text-white',
-    'hover:bg-red-300',
-    'focus:outline-none',
-    'focus:shadow-outline'
+    "decrease",
+    "px-3",
+    "py-1",
+    "rounded",
+    "bg-red-700",
+    "text-white",
+    "hover:bg-red-300",
+    "focus:outline-none",
+    "focus:shadow-outline"
   );
-  decrease.innerText = '-';
-  decrease.addEventListener('click', () => {
+  decrease.innerText = "-";
+  decrease.addEventListener("click", () => {
     const currentValue = parseInt(input.value);
     if (currentValue) {
       input.value = parseInt(input.value) - 1;
@@ -149,32 +149,32 @@ export const createEvent = ({
   actions.appendChild(quantity);
   eventDiv.appendChild(actions);
 
-  const addToCart = document.createElement('button');
+  const addToCart = document.createElement("button");
   addToCart.classList.add(
-    'add-to-cart-btn',
-    'px-4',
-    'py-2',
-    'rounded',
-    'text-white',
-    'font-bold',
-    'disabled:opacity-50',
-    'disabled:cursor-not-allowed',
-    'mt-4',
-    'focus:outline-none',
-    'focus:shadow-outline'
+    "add-to-cart-btn",
+    "px-4",
+    "py-2",
+    "rounded",
+    "text-white",
+    "font-bold",
+    "disabled:opacity-50",
+    "disabled:cursor-not-allowed",
+    "mt-4",
+    "focus:outline-none",
+    "focus:shadow-outline"
   );
-  addToCart.innerText = 'Add To Cart';
+  addToCart.innerText = "Add To Cart";
   addToCart.disabled = true;
 
-  addToCart.addEventListener('click', () => {
+  addToCart.addEventListener("click", () => {
     const ticketType = document.querySelector(`.${title}-ticket-type`).value;
     const quantity = input.value;
 
     if (parseInt(quantity)) {
-      fetch('/api/purchase', {
-        method: 'POST',
+      fetch("/api/purchase", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           ticketType: +ticketType,
@@ -187,11 +187,11 @@ export const createEvent = ({
           addPurchase(data); // Call the updated addPurchase function
           input.value = 0;
           addToCart.disabled = true;
-          toastr.success('Success!');
+          toastr.success("Success!");
         })
         .catch((error) => {
-          console.error('Error saving purchased event:', error);
-          toastr.error('Error!');
+          console.error("Error saving purchased event:", error);
+          toastr.error("Error!");
         });
     } else {
       // Handle the case when quantity is not a valid number
